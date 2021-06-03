0e0a6594aeed6f07494bab0b86cae29f1a322698
fmomoreira <felipemoreira.frontend@gmail.com>
2021-06-03 18:02:37 -0300

    construindo uma calculadora com typeScript

---
+++ calculadora/assets/scripts/calculadora/CalculadoraControle.js
@@ -0,0 +1,7 @@
+import DataHora from "./DataHora.js";
+export default class CalculadoraControle {
+    constructor() {
+        new DataHora();
+    }
+}
+//# sourceMappingURL=CalculadoraControle.js.map
