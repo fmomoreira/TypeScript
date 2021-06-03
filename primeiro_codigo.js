fc509a9192a149c651bf51a606fc1b324e37a5cc
fmomoreira <felipemoreira.frontend@gmail.com>
2021-06-03 16:11:51 -0300

    entendendo como o type escript nos ajuda no entendimento das variaveis criadas em duplicidade com alerta de erros

--- primeiro_codigo.js
+++ primeiro_codigo.js
@@ -1,1 +1,4 @@
 console.log("Hellow World");
+console.log("===================");
+var nome = "Joao";
+console.log("Olá " + nome + ". Seja bem vindo!");
--- primeiro_codigo.ts
+++ primeiro_codigo.ts
@@ -1,1 +1,8 @@
 console.log("Hellow World");
+
+console.log("===================");
+
+const nome = "Joao";
+
+console.log("Olá " + nome + ". Seja bem vindo!");
+
