b5b73f3169af8dbd45f73f78f05a0162c187d72f
fmomoreira <felipemoreira.frontend@gmail.com>
2021-06-03 16:19:27 -0300

    criando classes e compilando pra Es3

--- primeiro_codigo.js
+++ primeiro_codigo.js
@@ -2,3 +2,12 @@ console.log("Hellow World");
 console.log("===================");
 var nome = "Joao";
 console.log("Olá " + nome + ". Seja bem vindo!");
+console.log("===================");
+console.log("Criando class com TypeScript");
+var Produto = /** @class */ (function () {
+    function Produto(produtoNome, produtoValor) {
+        this.nome = produtoNome;
+        this.valor = produtoValor;
+    }
+    return Produto;
+}());
--- primeiro_codigo.ts
+++ primeiro_codigo.ts
@@ -6,3 +6,16 @@ const nome = "Joao";
 
 console.log("Olá " + nome + ". Seja bem vindo!");
 
+console.log("===================");
+console.log("Criando class com TypeScript");
+
+class Produto {
+  nome: string;
+  valor: number;
+  constructor(produtoNome, produtoValor) {
+    this.nome = produtoNome;
+    this.valor = produtoValor;
+
+
+  }
+}
